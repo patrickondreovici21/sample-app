5506dda3920644d26b6a94b09b95915a459b7c29
Patrick Ondreovici <patrickondreovici@192.168.1.4>
2025-09-07 09:38:03 +0300

    partial for edit/new form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,11 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
