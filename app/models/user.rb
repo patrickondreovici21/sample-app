c2f19ebd706fca2a78c76743c07ce787553e3dae
Patrick Ondreovici <patrickondreovici@192.168.1.4>
2025-09-09 15:50:15 +0300

    follow feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
     has_many :microposts, dependent: :destroy
-
+    has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+    has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+    has_many :following, through: :active_relationships, source: :followed
+    has_many :followers, through: :passive_relationships, source: :follower
     attr_accessor :remember_token, :activation_token
 
     before_save {self.email = email.downcase}
@@ -14,8 +17,13 @@ class User < ApplicationRecord
     has_secure_password
 
     def feed
-        Micropost.where("user_id = ?", id)
+        part_of_feed = "relationships.follower_id = :id OR microposts.user_id = :id"
+        Micropost.left_outer_joins(user: :followers)
+                 .where(part_of_feed, { id: id })
+                 .distinct
+                 .includes(:user, image_attachment: :blob)
     end
+      
 
 
     def User.new_token
@@ -50,6 +58,21 @@ class User < ApplicationRecord
     def session_token
         remember_digest || remember
     end
+
+
+    # Follows a user.
+    def follow(other_user)
+        following << other_user unless self == other_user
+    end
+    # Unfollows a user.
+    def unfollow(other_user)
+        following.delete(other_user)
+    end
+    # Returns true if the current user is following the other user.
+    def following?(other_user)
+        following.include?(other_user)
+    end
+
 
     private
 
